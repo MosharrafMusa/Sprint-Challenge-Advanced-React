926faac3ef942f225236da3b3fd119bd4dd8757b
Mosharraf Musa <mosharrafny@gmail.com>
2020-04-17 21:08:58 -0400

    did some stretch

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 
 import PlayerCard from "./components/PlayerCard";
 import Header from "./components/Header";
+import Chart from "./components/Chart";
 
 class App extends React.Component {
   state = {
@@ -17,11 +18,32 @@ class App extends React.Component {
       .catch((err) => console.log(err));
   }
 
+  playerCountByCountries = () => {
+    let countries = {};
+
+    this.state.players.forEach((player) => {
+      if (player.country in countries) {
+        countries[player.country] = countries[player.country] + 1;
+      } else {
+        countries[player.country] = 0;
+      }
+    });
+
+    return Object.keys(countries).map((country) => {
+      return { country, count: countries[country] };
+    });
+  };
+
   render() {
+    console.log(this.playerCountByCountries());
     return (
       <div className="App">
         <Header />
-
+        <Chart
+          data={this.playerCountByCountries()}
+          xAxisDataKey="country"
+          barDataKey="count"
+        />
         {this.state.players.map((player) => (
           <PlayerCard key={player.id} {...player} />
         ))}
